80a516e335b567d7eeefa50bbb0b174fb61612eb
oliveiralecca <oliveiraslc@yahoo.com.br>
2021-11-25 17:42:16 -0300

    testing eslint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import styles from './App.module.scss'
 import { LoginBox } from './components/LoginBox'
 import { MessageList } from './components/MessageList'
